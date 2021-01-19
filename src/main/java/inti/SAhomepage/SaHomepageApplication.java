16fa23836ae43d6e67fdf9f1a878604c5ca159c8
FloralLife <ocard9611@naver.com>
2021-01-19 13:20:16 +0900

    initial commit

---
+++ src/main/java/inti/SAhomepage/SaHomepageApplication.java
@@ -0,0 +1,13 @@
+package inti.SAhomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaHomepageApplication.class, args);
+	}
+
+}
