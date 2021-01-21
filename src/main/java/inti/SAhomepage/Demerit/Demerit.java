b0a794a7f7c6408e7a02e3d6391eee7d82d13678
yeonha99 <rhdqn1207@naver.com>
2021-01-21 13:35:54 +0900

    Repository 생성

---
+++ src/main/java/inti/SAhomepage/Demerit/Demerit.java
@@ -0,0 +1,41 @@
+package inti.SAhomepage.Demerit;
+
+public class Demerit {
+    private int num;
+    private int id;
+    private Float value;
+    private String reason;
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Float getValue() {
+        return value;
+    }
+
+    public void setValue(Float value) {
+        this.value = value;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
---
+++ src/main/java/inti/SAhomepage/Demerit/DemeritRepository.java
@@ -0,0 +1,12 @@
+package inti.SAhomepage.Demerit;
+import inti.SAhomepage.Demerit.Demerit;
+import java.util.List;
+import java.util.Optional;
+public interface DemeritRepository {
+    Demerit save(Demerit demerit);
+    Optional<Demerit> findByid(int id);
+    Optional<Demerit> findBynum(int num);
+    Optional<Demerit> findByFloat(Float value);
+    Optional<Demerit> findByreason(String reason);
+    List<Demerit> findAll();
+}
